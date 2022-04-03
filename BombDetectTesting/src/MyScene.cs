01eeb2d4970534a370570f48f5c34aa82c1684b5
carlton <carltonrbx@outlook.com>
2022-04-02 18:10:04 -0600

    testing project

---
+++ BombDetectTesting/src/MyScene.cs
@@ -0,0 +1,11 @@
+using BombDetect.Core;
+
+namespace BombDetectTesting;
+
+public class MyScene : Scene
+{
+	public MyScene()
+	{
+		Console.WriteLine("Scene started");
+	}
+}
---
+++ BombDetectTesting/src/Program.cs
@@ -0,0 +1,18 @@
+﻿using BombDetect;
+using BombDetect.Core;
+using static SDL2.SDL;
+
+namespace BombDetectTesting;
+
+internal static class Program
+{
+	private static void Main(string[] args)
+	{
+		Engine.Initialize("BombDetect Testing", 0, 0, 800, 600, SDL_WindowFlags.SDL_WINDOW_SHOWN);
+
+		// add all scenes to avoid null reference
+		SceneManager.AddScene("MyScene", new MyScene());
+
+		Engine.Run();
+	}
+}
